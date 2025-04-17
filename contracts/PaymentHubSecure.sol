5156fd78ba1980697e86374a266edc1053a3ac5f
Sami Belhaj <sami.belhadj@gmail.com>
2025-04-17 07:01:12 +0100

    Create PaymentHubSecure.sol

---
+++ contracts/PaymentHubSecure.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract PaymentHubSecure is Ownable, ReentrancyGuard, Pausable, AccessControl {
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
+    mapping(address => uint256) public balances;
+
+    event PaymentSent(address indexed from, address indexed to, uint256 amount);
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(OPERATOR_ROLE, msg.sender);
+    }
+
+    function deposit() external payable whenNotPaused {
+        balances[msg.sender] += msg.value;
+    }
+
+    function sendPayment(address payable to, uint256 amount)
+        external
+        nonReentrant
+        whenNotPaused
+    {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        to.transfer(amount);
+
+        emit PaymentSent(msg.sender, to, amount);
+    }
+
+    function pauseHub() external onlyRole(OPERATOR_ROLE) {
+        _pause();
+    }
+
+    function resumeHub() external onlyRole(OPERATOR_ROLE) {
+        _unpause();
+    }
+}
